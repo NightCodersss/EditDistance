5d289c9ffb4339f3b3208290284135e841405be7
justanothercoder <yanushviktor@gmail.com>
2015-02-24 17:09:55 +0300

    made convenient interface to python

--- trans.py
+++ trans.py
@@ -1,3 +1,26 @@
 from ctypes import cdll, py_object
 libtrans = cdll.LoadLibrary('./libtrans.so')
 libtrans.getTransducerNextMinPath.restype = py_object
+
+class Transducer:
+    
+    def __init__(self, trans = None):
+        self.trans = trans
+        self.resetMinPaths()
+    
+    @staticmethod
+    def fromRegexp(regex):
+        return Transducer(libtrans.transducerFromRegexp(regex))
+
+    def readFromFile(self, filename):
+        libtrans.readTransducerFromFile(self.trans, filename)
+
+    def visualize(self):
+        libtrans.visualizeTransducer(self.trans)
+
+    def resetMinPaths(self):
+        libtrans.resetTransducerMinPaths(self.trans)
+
+    def getNextMinPath(self):
+        return libtrans.getTransducerNextMinPath(self.trans)
+    
