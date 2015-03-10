d93f7e85984c6fc1cde1e3511fc298d15eda8cf8
Georgy Borisenko <gosha-bor@yandex.ru>
2015-03-10 20:04:04 +0300

    Updated, but it still does not work

--- test_regex.py
+++ test_regex.py
@@ -1,11 +1,25 @@
 import gen_regex
-import trans
+import re
+import rstr
+from trans import Transducer
 
 def test_random_regex():
     r = gen_regex.gen_regex()
-    for _ in range(50):
-        t = Transducer.fromRegexp(r);
-        s = rstr.xeger(r);
-        t.composition(Transducer.fromRegexp(s)).
+    print r
+    f = True;
+    for _ in range(3):
+        s = rstr.xeger(r).encode('utf-8')
+        if not Transducer.fromRegexp(r.encode('utf-8')).composition(Transducer.fromRegexp(s)).isEmpty():
+            print "pizdoh on "+s
+            f = False
+
+    print "second part"
+    for _ in range(3):
+        s = rstr.xeger("[a-z]*")
+        if not Transducer.fromRegexp(r.encode('utf-8')).composition(Transducer.fromRegexp(s.encode('utf-8'))).isEmpty() == (re.match(r, s).group() == ''):
+            print "pizdoh on "+s
+            f = False
+
+    return f
         
 
