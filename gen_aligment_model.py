b68d8de3076895a2bcf383c4bc99877f905f90da
justanothercoder <yanushviktor@gmail.com>
2015-03-23 19:28:44 +0300

    added russian automat and war and peace text

--- gen_aligment_model.py
+++ gen_aligment_model.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
-f = open("tbig.am", "w");
-letters = map(chr, range(ord('a'), ord('z')+1))
-letters.append(u'ε'.encode('utf-8'))
 
-f.write("{}\n".format(len(letters)**2))
+letters = []
 
-for ch1 in letters:
-    for ch2 in letters:
-        f.write("{} {} {}\n".format(ch1, ch2, int(ch1 != ch2)))
+def gen_letters(filename):
+    s = set(open(filename, 'r').read().decode('utf-8'))
+    s.remove(u'\n')
+    s.remove(u' ')
+    s.remove(u'\t')
+    s.add(u'\\n')
+    s.add(u'\\s')
+    s.add(u'\\t')
+    s.add(u'ε')
+    return s
 
-f.close()
+#letters = map(chr, range(ord('a'), ord('z')+1))
+#letters.append(u'ε'.encode('utf-8'))
+
+def gen_model(filename, letters):
+
+    f = open(filename, "w");
+    f.write("{}\n".format(len(letters)**2))
+
+    for ch1 in letters:
+        for ch2 in letters:            
+            f.write("{} {} {}\n".format(ch1.encode('utf-8'), ch2.encode('utf-8'), int(ch1 != ch2)))
+
+
+    f.close()
+
+gen_model('trussianandpunct.am', gen_letters('warandpeace.txt'))
